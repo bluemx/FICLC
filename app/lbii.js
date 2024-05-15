2dcd050106e7a69bd5b3a60264483dd1f904dec5
Albin Rodriguez <ealbinu@gmail.com>
2024-05-15 13:59:31 -0600

    update 1 venues

--- app/lbii.js
+++ app/lbii.js
@@ -102,63 +102,113 @@ var lbii_films = [
   var lbii_venues = [
     {
         id: 1,
-        name: 'Teatro de la Marina',
-        description: 'Un elegante teatro ubicado en el corazón de la Marina de Cabo San Lucas, con capacidad para 150 personas.',
-        description_en: 'An elegant theater located in the heart of Cabo San Lucas Marina, with a seating capacity of 150 people.',
+        name: 'Centro Estatal de Las Artes Tijuana (CEART)',
+        description: '<p>Inaugurado el 19 de abril de 2013, el CEART consta de un complejo arquitectónico integrado por seis edificios y una explanada con capacidad para recibir a 2,000 personas. Su misión es contribuir a la construcción de una sociedad con mayor sentido, en respeto a las diferentes expresiones multiculturales y étnicas, en un clima de tolerancia, aceptación, equidad y justicia, así como implementar modelos educativos de excelencia, para el desarrollo profesional de artistas, creadores, gestores culturales, promotores y docentes de arte.</p><p>Su programación artística consta de un Foro Experimental para la realización de conferencias, obras de teatro, danza y eventos musicales. También cuenta con el espacio <strong>Cinema CEART Tijuana</strong>, donde constantemente ofrece proyecciones de cine de manera gratuita.</p>',
+        description_en: '---',
         calendarLink: 'https://example.com/calendar/teatro-de-la-marina',
-        address: 'Calle del Paseo Marítimo, #15, Colonia Marina, Cabo San Lucas, BCS 23450, México',
-        googleMapsLink: 'https://goo.gl/maps/abc123',
-        imageName: 'teatro_de_la_marina.jpg'
+        address: 'Vía Rápida Ote. 15320, Rio Tijuana 3ra Etapa, 22105 Tijuana, B.C.',
+        googleMapsLink: 'https://maps.app.goo.gl/NDd8RCj4mfQGUs2V9',
+        imageName: 'ceart.jpg'
     },
     {
         id: 2,
-        name: 'Centro Cultural Cabo',
-        description: 'Un espacio cultural vibrante que ofrece una variedad de eventos y actividades para la comunidad local, incluyendo proyecciones de cine, exposiciones de arte y presentaciones musicales.',
-        description_en: 'A vibrant cultural space that offers a variety of events and activities for the local community, including film screenings, art exhibitions, and musical performances.',
+        name: 'Centro Cultural Tijuana (CECUT)',
+        description: '<p>El CECUT es un centro cultural de México en la frontera, fundado el 20 de octubre de 1982. Tiene el compromiso de satisfacer las necesidades artísticas y culturales de la región norte del país, a través de programas que incluyen diversas manifestaciones artísticas y temas de la cultura tradicional y contemporánea.</p><p>Sus instalaciones se integran de la siguiente manera: Domo IMAX, Museo de las Californias, Sala de Espectáculos, El Cubo (tres salas de estándares internacionales), Salas de Exposiciones Temporales, <strong>Cineteca</strong>, Acuario, Salones de Ensayo, entre otras. Sumando una superficie de 3.5 hectáreas, ubicado a dos kilómetros de la frontera con Estados Unidos, es el símbolo de mayor reconocimiento con que se identifica a Tijuana y a los tijuanenses.</p>',
+        description_en: '---',
         calendarLink: 'https://example.com/calendar/centro-cultural-cabo',
-        address: 'Calle de la Cultura, #20, Centro, Cabo San Lucas, BCS 23410, México',
-        googleMapsLink: 'https://goo.gl/maps/def456',
-        imageName: 'centro_cultural_cabo.jpg'
+        address: 'P.º de los Héroes 9350, Zona Urbana Rio Tijuana, 22010 Tijuana, B.C.',
+        googleMapsLink: 'https://maps.app.goo.gl/ckPHfpkpEuBiJMUy8',
+        imageName: 'cecut.jpg'
     },
     {
         id: 3,
-        name: 'Sala Bahía',
-        description: 'Un acogedor salón de eventos con vistas panorámicas al mar, ideal para proyecciones de cine al aire libre y eventos especiales.',
-        description_en: 'A cozy event hall with panoramic views of the sea, ideal for outdoor film screenings and special events.',
+        name: 'Cinema Vagabundo',
+        description: '<p>Cineclub fundado en agosto del 2011. Ubicado en el Pabellón Cultural de Cabo San Lucas, su propósito es brindar un espacio a películas independientes o de poca difusión en salas comerciales. Uno de sus objetivos principales es incentivar la creación de materiales audiovisuales locales; como cortometrajes, cineminutos, mediometrajes y largometrajes de ficción, así como documentales. El nombre de vagabundo, surge de su esencia itinerante.</p><p>A lo largo de los ya casi 13 años de exhibición, ha contado con la presencia de actores  como: Joaquín Cosío, Tenoch Huerta, Arcelia Ramirez y recientemente Adan Jodorowsky. Los creadores de Cinema Vagabundo también son fundadores de CEDECINE, una comunidad nacional de cineclubes conformada en los estudios churubusco.</p><p>Han proyectado en gran parte de Baja California Sur; San José del Cabo, Todos Santos, San Javier, Loreto, Mulegé, San Francisco de la Sierra, San Ignacio, entre otros.</p>',
+        description_en: '---',
         calendarLink: 'https://example.com/calendar/sala-bahia',
-        address: 'Avenida de la Bahía, #5, Cabo San Lucas, BCS 23453, México',
-        googleMapsLink: 'https://goo.gl/maps/ghi789',
-        imageName: 'sala_bahia.jpg'
+        address: 'Blvd. Paseo de la Marina, Centro, 23450 Cabo San Lucas, B.C.S.',
+        googleMapsLink: 'https://maps.app.goo.gl/NswKTbRnAaqgqHu86',
+        imageName: 'cinema_vagabundo.jpg'
     },
     {
         id: 4,
-        name: 'Auditorio Costero',
-        description: 'Un impresionante auditorio al aire libre situado junto a la costa, que ofrece una experiencia única de cine bajo las estrellas.',
-        description_en: 'An impressive open-air auditorium located by the coast, offering a unique cinema experience under the stars.',
+        name: 'MUABCS (Museo de Arte de Baja California Sur)',
+        description: '<p>El Museo de Arte de Baja California Sur es el primer espacio en el territorio sudcaliforniano dedicado y equipado para recibir, montar y mostrar arte moderno y arte contemporáneo, además de la sala dedicada a la plástica sudcaliforniana. Sus instalaciones se encuentran en lo que fue la primera Casa de Gobierno reconstruida en 1981. A partir de 2019 se inician los trabajos de ampliación para albergar al MUABCS.</p><p>En coordinación con el Museo de Arte Moderno de la Ciudad de México, el MUABCS se inauguró con la exposición “Paisajes”, colección MAM. Esta exposición se conformó con la obra de reconocidos artistas como Manuel Álvarez Bravo, Rufino Tamayo, Juan Soriano, Carlos Mérida, entre otros; de la generación de La Ruptura se encuentran Lilia Carrillo, Gunther Gerzso y Vicente rojo; una foto icónica de Graciela Iturbide y autores contemporáneos como Helen Escobedo, Gabriel Orozco, Carla Rippey y Daniel Lezama.</p><p><strong>Cuenta con una sala audiovisual para 59 personas.</strong></p>',
+        description_en: '---',
         calendarLink: 'https://example.com/calendar/auditorio-costero',
-        address: 'Paseo del Litoral, #10, Cabo San Lucas, BCS 23456, México',
-        googleMapsLink: 'https://goo.gl/maps/jkl012',
-        imageName: 'auditorio_costero.jpg'
+        address: 'Cl. 5 de Mayo SN-S"BANCO VITAL", Zona Central, 23000 La Paz, B.C.S.',
+        googleMapsLink: 'https://maps.app.goo.gl/SnCFQb4Knn6Yit9F7',
+        imageName: 'muabcs.jpg'
     },
     {
         id: 5,
-        name: 'Plaza del Arte',
-        description: 'Una encantadora plaza rodeada de galerías de arte y estudios de artistas, donde se celebran proyecciones de cine al aire libre y eventos culturales.',
-        description_en: 'A charming square surrounded by art galleries and artist studios, where outdoor film screenings and cultural events are held.',
+        name: 'Museo del Vaquero de las Californias',
+        description: '<p>MUVACA es un espacio que muestra historias cruciales de los orígenes de la cultura vaquera y ranchera en las Californias. Este museo se adentra en el estilo de vida e identidad de los vaqueros, así como en las tradiciones que se mantienen vivas hasta nuestros días gracias a las familias que viven en distintas regiones de Baja California Sur.</p><p>Facebook: Museo del Vaquero de las Californias</p><p>Instagram: @museo_muvaca</p>',
+        description_en: '---',
         calendarLink: 'https://example.com/calendar/plaza-del-arte',
-        address: 'Calle de la Artesanía, #30, Centro, Cabo San Lucas, BCS 23412, México',
-        googleMapsLink: 'https://goo.gl/maps/mno345',
-        imageName: 'plaza_del_arte.jpg'
+        address: 'Calle Ayuntamiento, Minero Núm. 1 esq, 23210 El Triunfo, B.C.S.',
+        googleMapsLink: 'https://maps.app.goo.gl/Qbfa4DriNu1qydSz5',
+        imageName: 'museo_vaquero_californias.jpg'
     },
     {
         id: 6,
-        name: 'Salón de Pueblo',
-        description: 'Un salón comunitario multifuncional que sirve como centro de reunión para eventos locales, incluyendo proyecciones de cine, conciertos y fiestas.',
-        description_en: 'A multifunctional community hall that serves as a gathering place for local events, including film screenings, concerts, and parties.',
+        name: 'Museo Ruta de Plata',
+        description: '<p>El Museo Ruta de Plata Centro Regional Interpretativo está dedicado a preservar y compartir la historia minera de Baja California Sur, así como las migraciones, vivencias de las personas que han habitado el lugar, desde los primeros colonos, a mediados de los años 1500, hasta los años del auge minero de la región.</p><p>Facebook: Museo Ruta de Plata Centro Regional Interpretativo</p><p>Instagram: @museorutadeplata</p>',
+        description_en: '---',
         calendarLink: 'https://example.com/calendar/salon-de-pueblo',
-        address: 'Calle de la Comunidad, #40, Pueblo Nuevo, Cabo San Lucas, BCS 23478, México',
-        googleMapsLink: 'https://goo.gl/maps/pqr901',
-        imageName: 'salon_de_pueblo.jpg'
+        address: 'Calle Ayuntamiento (Progreso y, Gral. M. Márquez de León, Centro, 23210 El Triunfo, B.C.S.',
+        googleMapsLink: 'https://maps.app.goo.gl/MzAKrBTiKW3psd2v8',
+        imageName: 'museo_ruta_plata.jpg'
+    },
+    {
+        id: 7,
+        name: 'Playa Costa Azul',
+        description: '<p>La Playa Costa Azul es uno de los lugares favoritos de los amantes del surf. Corre a lo largo de la Carretera Transpeninsular en la costa suroeste de San José del Cabo, lo que la convierte en un lugar privilegiado para que la Liga Mundial de Surf realice su apertura anual todos los años en junio. También es un hermoso lugar para practicar snorkel o simplemente para disfrutar de una caminata en la playa.</p>',
+        description_en: '---',
+        calendarLink: 'https://example.com/calendar/salon-de-pueblo',
+        address: 'Baja California Sur',
+        googleMapsLink: 'https://maps.app.goo.gl/cL143xmGiBGPXW4EA',
+        imageName: 'playa_costa_azul.jpg'
+    },
+    {
+        id: 8,
+        name: 'Plaza Mijares',
+        description: '<p>Ubicada en el corazón de San José del Cabo, Plaza Mijares está rodeada de edificios históricos como el Ayuntamiento, la iglesia de San José y la Casa de la Cultura, además del atractivo kiosko, la asta bandera y el jardín de caballeros ilustres. También cuenta con un anfiteatro al aire libre donde se llevan a cabo una variedad de eventos culturales. Es conocida por los lugareños como La Plaza y es un punto de encuentro popular entre los residentes y turistas.</p>',
+        description_en: '---',
+        calendarLink: 'https://example.com/calendar/salon-de-pueblo',
+        address: 'Blvd. Antonio Mijares, Centro, 23400 San José del Cabo, B.C.S.',
+        googleMapsLink: 'https://maps.app.goo.gl/w7X2AbczvqQe7WKRA',
+        imageName: 'plaza_mijares.jpg'
+    },
+    {
+        id: 9,
+        name: 'Universidad Autónoma de Baja California Sur (UABCS)',
+        description: '<p>Creada el 31 de diciembre de 1975, la Universidad Autónoma de Baja California Sur (UABCS) es la institución de educación superior con más variedad disciplinar y de niveles educativos en Sudcalifornia al ofrecer programas de tecnicatura superior universitaria, licenciaturas, ingenierías, maestrías y doctorados en las áreas de conocimiento de ciencias sociales y humanidades, ciencias del mar y de la tierra, ciencias agropecuarias, y sistemas computacionales.</p><p>En el ámbito de la docencia, la Universidad ha aumentado y diversificado significativamente sus servicios educativos en los cinco municipios de la entidad a través del campus La Paz y cuatro extensiones académicas: Guerrero Negro, Loreto, Ciudad Insurgentes, y Los Cabos.</p><p><strong>Cuenta con una sala de cine</strong>, un espacio creado para que los y las estudiantes puedan disfrutar de una película, que comúnmente no podrían encontrar en otros cines comerciales. Y siempre aprender cosas nuevas con cada proyección.</p><p>Facebook: Sala de cine UABCS</p><p>Instagram: UABCS_sala de cine</p>',
+        description_en: '---',
+        calendarLink: 'https://example.com/calendar/salon-de-pueblo',
+        address: 'Avenida de la Juventud 17, Juárez, 23460 Cabo San Lucas, B.C.S.',
+        googleMapsLink: 'https://maps.app.goo.gl/XBchtNXNJ5tFUnjd6',
+        imageName: 'uabcs.jpg'
+    },
+    {
+        id: 10,
+        name: 'Universidad de Tijuana',
+        description: '',
+        description_en: '---',
+        calendarLink: 'https://example.com/calendar/salon-de-pueblo',
+        address: 'Parcela No. 4772 P 1/3 Km. 6.5 Carretera Cabo San Lucas – San José del Cabo El Tezal, 23400 Cabo San Lucas, B.C.S.',
+        googleMapsLink: 'https://maps.app.goo.gl/pfusZMiEz4ert6nQ6',
+        imageName: 'universidad_tijuana.jpg'
+    },
+    {
+        id: 11,
+        name: 'Cineclub El Nido',
+        description: '',
+        description_en: '---',
+        calendarLink: 'https://example.com/calendar/salon-de-pueblo',
+        address: 'Baja California Sur',
+        googleMapsLink: 'https://maps.app.goo.gl/T2syBchnCvd6BWV29',
+        imageName: 'cineclub_el_nido.jpg'
     }
   ];
 
